1aa778daeef2f9d16548412e7843da8ef984db8c
zhongzhi107 <zhong.zhi@163.com>
2017-06-13 07:16:56 +0800

    chore: 优化代码结构

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-var fs = require('fs');
-var path = require('path');
-var url = require('url');
-var util = require('util');
-var assign = require('object-assign-deep');
-var clearRequire = require('clear-require');
-var pug = require('pug');
+import assign from 'object-assign-deep';
+import pug from 'pug';
+import { getPath, getContext } from 'packing-template-util';
 
 module.exports = function(options) {
   options = assign({
@@ -17,48 +13,11 @@ module.exports = function(options) {
     pretty: false // 是否输出带缩进格式的html
   }, options);
   return async (req, res, next) => {
-    var urlObject = url.parse(req.url);
-    var pathname = options.rewriteRules[urlObject.pathname] || urlObject.pathname;
-    var templateAbsPath = path.resolve(path.join(options.templates, pathname));
-    var dataAbsPath = path.resolve(path.join(options.mockData, pathname.replace(options.extension, '.js')));
-    var globalDataPath = path.resolve(path.join(options.mockData, options.globalData));
-    if (fs.existsSync(templateAbsPath)) {
-      var globalContext = {};
-      if (fs.existsSync(globalDataPath)) {
-        clearRequire(globalDataPath);
-        var gcontext = require(globalDataPath);
-        if (util.isFunction(gcontext)) {
-          globalContext = gcontext(req, res);
-          if (globalContext instanceof Promise) {
-            await globalContext.then((data) => {
-              globalContext = data;
-            }, (error) => {
-              console.error(error);
-            });
-          }
-        } else {
-          globalContext = gcontext;
-        }
-      }
-      var pageContext = {};
-      if (fs.existsSync(dataAbsPath)) {
-        clearRequire(dataAbsPath);
-        var pcontext = require(dataAbsPath);
-        if (util.isFunction(pcontext)) {
-          pageContext = pcontext(req, res);
-          if (pageContext instanceof Promise) {
-            await pageContext.then((data) => {
-              pageContext = data;
-            }, (error) => {
-              console.error(error);
-            });
-          }
-        } else {
-          pageContext = pcontext;
-        }
-      }
+    const { templatePath, pageDataPath, globalDataPath } = getPath(req, options);
+    if (existsSync(templatePath)) {
+      const context = getContext(req, res, pageDataPath, globalDataPath);
       try {
-        var output = pug.renderFile(templateAbsPath, assign(options, globalContext, pageContext));
+        const output = pug.renderFile(templatePath, assign(options, context));
         res.end(output);
       } catch (e) {
         console.log(e);
